1dc3c63556e0755ba6cad91a0027faa08c92653e
Laura Hall <laura@allthehalls.com>
2024-11-07 10:24:57 +0000

    Everything updated

---
+++ sql-crud.py
@@ -0,0 +1,27 @@
+from sqlalchemy import (
+    create_engine, Column, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# executing the instructions from the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+
+# create a class-based model for the "Programmer" table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id= Column(Integer, primary_key=True)
+
+
+
+# instead of connecting to the database directly, we will ask for a session
+# create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
+# opens an actual session by calling the Session() subclass defined above
+session = Session()
+
+# creating the database using declarative_base subclass
+base.metadata.create_all(db)
